359a3d4c67fd11ff9aaee826fe1765b5f4ee6a11
Wujidadi <wujidadi@gmail.com>
2022-04-26 11:01:54 +0800

    引入色碼 library

---
+++ lib/Colors.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Lib;
+
+/**
+ * 顏色名稱與 16 進位色碼定義映射表
+ */
+class Colors
+{
+    # 粉紅色系
+    const MediumVioletRed = '#C71585';
+    const DeepPink = '#FF1493';
+    const PaleVioletRed = '#DB7093';
+    const HotPink = '#FF69B4';
+    const LightPink = '#FFB6C1';
+    const Pink = '#FFC0CB';
+
+    # 紅色系
+    const DarkRed = '#8B0000';
+    const Red = '#FF0000';
+    const Firebrick = '#B22222';
+    const Crimson = '#DC143C';
+    const IndianRed = '#CD5C5C';
+    const LightCoral = '#F08080';
+    const Salmon = '#FA8072';
+    const DarkSalmon = '#E9967A';
+    const LightSalmon = '#FFA07A';
+
+    # 橘色系
+    const OrangeRed = '#FF4500';
+    const Tomato = '#FF6347';
+    const DarkOrange = '#FF8C00';
+    const Coral = '#FF7F50';
+    const Orange = '#FFA500';
+
+    # 黃色系
+    const DarkKhaki = '#BDB76B';
+    const Gold = '#FFD700';
+    const Khaki = '#F0E68C';
+    const PeachPuff = '#FFDAB9';
+    const Yellow = '#FFFF00';
+    const PaleGoldenrod = '#EEE8AA';
+    const Moccasin = '#FFE4B5';
+    const PapayaWhip = '#FFEFD5';
+    const LightGoldenrodYellow = '#FAFAD2';
+    const LemonChiffon = '#FFFACD';
+    const LightYellow = '#FFFFE0';
+
+    # 棕色系
+    const Maroon = '#800000';
+    const Brown = '#A52A2A';
+    const SaddleBrown = '#8B4513';
+    const Sienna = '#A0522D';
+    const Chocolate = '#D2691E';
+    const DarkGoldenrod = '#B8860B';
+    const Peru = '#CD853F';
+    const RosyBrown = '#BC8F8F';
+    const Goldenrod = '#DAA520';
+    const SandyBrown = '#F4A460';
+    const Tan = '#D2B48C';
+    const Burlywood = '#DEB887';
+    const Wheat = '#F5DEB3';
+    const NavajoWhite = '#FFDEAD';
+    const Bisque = '#FFE4C4';
+    const BlanchedAlmond = '#FFEBCD';
+    const Cornsilk = '#FFF8DC';
+
+    # 綠色系
+    const DarkGreen = '#006400';
+    const Green = '#008000';
+    const DarkOliveGreen = '#556B2F';
+    const ForestGreen = '#228B22';
+    const SeaGreen = '#2E8B57';
+    const Olive = '#808000';
+    const OliveDrab = '#6B8E23';
+    const MediumSeaGreen = '#3CB371';
+    const LimeGreen = '#32CD32';
+    const Lime = '#00FF00';
+    const SpringGreen = '#00FF7F';
+    const MediumSpringGreen = '#00FA9A';
+    const DarkSeaGreen = '#8FBC8F';
+    const MediumAquamarine = '#66CDAA';
+    const YellowGreen = '#9ACD32';
+    const LawnGreen = '#7CFC00';
+    const Chartreuse = '#7FFF00';
+    const LightGreen = '#90EE90';
+    const GreenYellow = '#ADFF2F';
+    const PaleGreen = '#98FB98';
+
+    # 青色系
+    const Teal = '#008080';
+    const DarkCyan = '#008B8B';
+    const LightSeaGreen = '#20B2AA';
+    const CadetBlue = '#5F9EA0';
+    const DarkTurquoise = '#00CED1';
+    const MediumTurquoise = '#48D1CC';
+    const Turquoise = '#40E0D0';
+    const Aqua = '#00FFFF';
+    const Cyan = '#00FFFF';
+    const Aquamarine = '#7FFFD4';
+    const PaleTurquoise = '#AFEEEE';
+    const LightCyan = '#E0FFFF';
+
+    # 藍色系
+    const Navy = '#000080';
+    const DarkBlue = '#00008B';
+    const MediumBlue = '#0000CD';
+    const Blue = '#0000FF';
+    const MidnightBlue = '#191970';
+    const RoyalBlue = '#4169E1';
+    const SteelBlue = '#4682B4';
+    const DodgerBlue = '#1E90FF';
+    const DeepSkyBlue = '#00BFFF';
+    const CornflowerBlue = '#6495ED';
+    const SkyBlue = '#87CEEB';
+    const LightSkyBlue = '#87CEFA';
+    const LightSteelBlue = '#B0C4DE';
+    const LightBlue = '#ADD8E6';
+    const PowderBlue = '#B0E0E6';
+
+    # 紫色系
+    const Indigo = '#4B0082';
+    const Purple = '#800080';
+    const DarkMagenta = '#8B008B';
+    const DarkViolet = '#9400D3';
+    const DarkSlateBlue = '#483D8B';
+    const RebeccaPurple = '#663399';
+    const BlueViolet = '#8A2BE2';
+    const DarkOrchid = '#9932CC';
+    const Fuchsia = '#FF00FF';
+    const Magenta = '#FF00FF';
+    const SlateBlue = '#6A5ACD';
+    const MediumSlateBlue = '#7B68EE';
+    const MediumOrchid = '#BA55D3';
+    const MediumPurple = '#9370DB';
+    const Orchid = '#DA70D6';
+    const Violet = '#EE82EE';
+    const Plum = '#DDA0DD';
+    const Thistle = '#D8BFD8';
+    const Lavender = '#E6E6FA';
+
+    # 白色系
+    const MistyRose = '#FFE4E1';
+    const AntiqueWhite = '#FAEBD7';
+    const Linen = '#FAF0E6';
+    const Beige = '#F5F5DC';
+    const WhiteSmoke = '#F5F5F5';
+    const LavenderBlush = '#FFF0F5';
+    const OldLace = '#FDF5E6';
+    const AliceBlue = '#F0F8FF';
+    const Seashell = '#FFF5EE';
+    const GhostWhite = '#F8F8FF';
+    const Honeydew = '#F0FFF0';
+    const FloralWhite = '#FFFAF0';
+    const Azure = '#F0FFFF';
+    const MintCream = '#F5FFFA';
+    const Snow = '#FFFAFA';
+    const Ivory = '#FFFFF0';
+    const White = '#FFFFFF';
+
+    # 灰黑色系
+    const Black = '#000000';
+    const DarkSlateGray = '#2F4F4F';
+    const DimGray = '#696969';
+    const SlateGray = '#708090';
+    const Gray = '#808080';
+    const LightSlateGray = '#778899';
+    const DarkGray = '#A9A9A9';
+    const Silver = '#C0C0C0';
+    const LightGray = '#D3D3D3';
+    const Gainsboro = '#DCDCDC';
+}
